392feed580468475d87a7b6655abb3fc1a018d83
AP <Alexanderp2026@student.cis.edu.hk>
2023-09-14 13:22:05 +0800

    <?xml version="1.0" encoding="UTF-8"?> <project version="4">   <component name="VcsDirectoryMappings">     <mapping directory="/Users/alexanderpak/IdeaProjects/Test" vcs="Git" />   </component> </project>

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
-        int myInt = 33;
-        int myInt2 = 46;
-        System.out.println(myInt + myInt2);
-        System.out.println("3" + 3);
-        System.out.println("3" + "3");
-        System.out.println("Hello world!" + "hello ");
+        System.out.println("Hello world!");
     }
+
 }
+
